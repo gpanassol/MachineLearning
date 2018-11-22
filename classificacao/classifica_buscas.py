3ce338e8d1098eab57d575231bfe2ee3ff0b99a9
Gabriel <gabriel.panassol@going2.com.br>
2018-11-22 14:20:22 -0200

    Aprendendo a ler CSV com biblioteca Panda pydata

--- classificacao/classifica_buscas.py
+++ classificacao/classifica_buscas.py
@@ -1,3 +1,14 @@
-from dados import carregar_buscas
-X,Y = carregar_buscas()
-print(Y)
+import pandas as pd
+
+df = pd.read_csv('busca.csv')
+X_df = df[['home', 'busca', 'logado']]
+Y_df = df['comprou']
+
+Xdummies_df = pd.get_dummies(X_df).astype(int)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+print(Xdummies_df)
+
