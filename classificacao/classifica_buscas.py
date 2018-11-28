c9f72ed7b34c7cfd99c62eddddc1269b3b3f3e58
Gabriel <gabriel.panassol@going2.com.br>
2018-11-28 14:06:22 -0200

    Melhorando o algoritmo

--- classificacao/classifica_buscas.py
+++ classificacao/classifica_buscas.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from collections import Counter
 
-df = pd.read_csv('busca.csv')
+df = pd.read_csv('busca_string.csv')
 X_df = df[['home', 'busca', 'logado']]
 Y_df = df['comprou']
 
@@ -15,8 +16,6 @@ porcentagem_treino = 0.9
 tamanho_de_treino = porcentagem_treino * len(Y)
 tamanho_de_teste = len(Y) - tamanho_de_treino
 
-print(tamanho_de_treino)
-
 treino_dados = X[:int(tamanho_de_treino)]
 treino_marcacoes = Y[:int(tamanho_de_treino)]
 
@@ -29,13 +28,16 @@ modelo.fit(treino_dados, treino_marcacoes)
 
 resultado = modelo.predict(teste_dados)
 
-diferencas = resultado - teste_marcacoes
+acertos = resultado == teste_marcacoes
 
-acertos = [d for d in diferencas if d == 0]
-total_de_acertos = len(acertos)
+total_de_acertos = sum(acertos)
 total_de_elementos = len(teste_dados)
 
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
-print(taxa_de_acerto)
-print(total_de_elementos)
+print("Taxa de acerto do algoritmo: %f" % taxa_de_acerto)
+print(total_de_elementos) 
+
+acerto_base = max(Counter(teste_marcacoes).itervalues())
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
